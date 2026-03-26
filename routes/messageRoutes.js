ee9db29f180f47d312e6b517a8790660d1fc41f7
Nguyen Chau Tuan <chautuan1708@gmail.com>
2026-03-26 16:02:17 +0700

    chỉnh sửa nhỏ ở messageRoute

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,7 +1,7 @@
 const { addMessage, getAllMessage} = require("../controllers/messageController");
 const router = require("express").Router();
 
-router.post("/addMessage", addMessage);
-router.post("/getAllMessages", getAllMessage);
+router.post("/addMessage/", addMessage);
+router.post("/getAllMessages/", getAllMessage);
 
 module.exports = router;
